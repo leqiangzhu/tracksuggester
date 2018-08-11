df83255cb833edf6d94f20d17dbfa3150f042356
Leqiangzhu（David） <41922877+leqiangzhu@users.noreply.github.com>
2018-08-11 16:35:45 -0700

    Update scripts.js
    "Modify the test logic, add the style, imgs, add the return button"

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,31 @@ $(document).ready(function() {
     $("#inputBox").submit(function(event) {
       var nameInput = $("input#studentName").val();
       var emailInput= $("input#email").val();
+	  var studentGender=$("#gender").val();
+	  var studentLocation=$("#local").val();
+	  
+	  //alert("studentGender"+studentGender)
+	  if(studentGender==="male"){
+		   $("#genderMale").show(); 
+		
+	  }else{
+		  $("#genderFemale").show(); 
+	  }
+	  
+	  if(studentLocation==="seattle"){
+		   $("#locationsettle").show(); 
+		
+	  }else{
+		  $("#locationportland").show(); 
+	  }
+	  
+	  
 
       $(".nameReturn").text(nameInput);
-      $(".emailReturn").text(emailInput);		
+      $(".emailReturn").text(emailInput);
+	  $(".GenderReturn").text(studentGender);
+	  
+		
 		
 		//for the answer , We need to calculate the score of 4 languages which 
 		//the user goto,delear 4 vales 
@@ -20,74 +42,89 @@ $(document).ready(function() {
 			$("#trackQueston").hide(); 
 
 		
-		//this is the 1st value of Question 1 
+		//delear  four values of suestion 
 		var valueOfQuestion_1 = $("input:radio[name=answerForQ1]:checked").val();
 		var valueOfQuestion_2 = $("input:radio[name=answerForQ2]:checked").val();
 		var valueOfQuestion_3 = $("input:radio[name=answerForQ3]:checked").val();
 		var valueOfQuestion_4 = $("input:radio[name=answerForQ4]:checked").val();
 		
+		
+				if(nameInput.length == 0||
+				valueOfQuestion_1.length == 0||
+				valueOfQuestion_2.length == 0||
+				valueOfQuestion_3.length == 0||
+				valueOfQuestion_4.length == 0){
+				alert("please enter your info")
+				//location.reload();
+				//break;
+				}
+      
+		
+		
+		//the value of Q1
 		if(valueOfQuestion_1==="0"){
-			scoreOfJava +=1;
-			scoreOfCsharp +=1;
 			scoreOfCss +=1;
 
-		} else{
+		} else if(valueOfQuestion_1==="1"){
 			scoreOfJava +=2;
 			scoreOfCsharp +=2;
-		
 		}
-		//question 2
+		
+		//the value of Q2
 		if(valueOfQuestion_2==="1"){
-			scoreOfJava +=1;
-			scoreOfCsharp +=1;
-			scoreOfCss +=1;
+			scoreOfJava +=3;
+			scoreOfCsharp +=3;
+			
 	
-		} else{
-			scoreOfJava +=2;
-			scoreOfCsharp +=2;
+		}else if(valueOfQuestion_2==="2"){
+			scoreOfCss +=2;
+		}else if(valueOfQuestion_2==="3"){
+			scoreOfCss +=2;
+		}
+		
+		//the value of Q3
+		if(valueOfQuestion_3==="1"){
+			scoreOfJava +=5;
+		} else if (valueOfQuestion_3==="2"){
+			scoreOfCss +=4;
+		}else if(valueOfQuestion_3==="3"){
+			scoreOfJava +=4;
+			scoreOfCsharp +=4;
+		}
+		//the value of Q4
+		if(valueOfQuestion_4==="1"){
+			scoreOfJava +=3;
+			scoreOfCsharp +=3;
+		} else if (valueOfQuestion_4==="2"||valueOfQuestion_4==="3"){
+			scoreOfCss +=3;
 		}
 		
 		
-		//question 3
-		if(valueOfQuestion_3==="3"){
-			scoreOfJava +=1;
-			
 		
-			
-		} else{
-			scoreOfJava +=2;
-			scoreOfCsharp +=2;
-		}
-		//question 4
-		if(valueOfQuestion_4==="2"){
-			
-			scoreOfCss +=100;
-		
-			
-		} else{
-			scoreOfJava +=2;
-			scoreOfCsharp +=2;
-
-		}
-		
+		 // alert("scoreOfJava:"+scoreOfJava);
+		 // alert("scoreOfCsharp:"+scoreOfCsharp);
+		 // alert("scoreOfCss:"+scoreOfCss);
 		
 		var max = Math.max(scoreOfJava, scoreOfCsharp, scoreOfCss);
+		//alert("MAX=:"+max);
 		
-		if(scoreOfJava=max){
-			
-			$("#recommendForStudnet").show(); 
+		if(scoreOfJava==max){
+			$("#suggestionOfJava").show(); 
 			
 		}
 		
-		if(scoreOfCsharp=max){
+		if(scoreOfCsharp==max){
 			$("#suggestionOfCsharp").show(); 
 			
 		}
-		if(scoreOfCss=max){
+		if(scoreOfCss==max){
 			$("#suggestionOfCss").show(); 
 			
 		}
-		
+		//try again
+			$("#tryAgain").click(function(){
+			location.reload();
+			});
 
 		
   })
